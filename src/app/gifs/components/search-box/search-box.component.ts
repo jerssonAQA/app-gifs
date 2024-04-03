128437603499433a9369ed865dcbc94eebff8ea2
Jersson_quintero <jersson.9.5@gmail.com>
2024-04-03 17:00:53 -0500

    agregando input de busqueda

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -6,12 +7,26 @@ import { Component } from '@angular/core';
     <h5>Buscar: </h5>
     <input type="text"
     class="form-control"
-    placeholder="Buscar gifs...">
+    placeholder="Buscar gifs ..."
+    (keyup.enter)="searchTag()"
+    #txtTagInput
+    >
   `
 })
 
 export class SearchBoxComponent {
-  constructor() { }
 
-  ngOnInit() { }
+  @ViewChild('txtTagInput')
+  public tagInput!:ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService:GifsService) { }
+
+  searchTag(){
+    const newTag=this.tagInput.nativeElement.value;
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value='';
+
+  }
 }
