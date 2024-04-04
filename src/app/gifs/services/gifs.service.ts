ebc2d45441f843dce4bcb053fcb6e9cc93754e3a
JerssonQuintero <jersson.9.5@gmail.com>
2024-04-03 23:17:56 -0500

    Agregando HTTPmodule al app-gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,40 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
   private _tagsHistory:string[]=[];
+  private apiKey      :string='Xyym62qHnDBENDm2lacPSIqu8UoAgH1G';
+  private serviceUrl  :string='https://api.giphy.com/v1/gifs';
 
-
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   get tagHistory(){
     return [...this._tagsHistory];
   }
 
+  private ordenizaHistory(tag:string){
+    tag = tag.toLowerCase();
+    if (this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter((oldTag)=> oldTag !==tag)
+  }
+  this._tagsHistory.unshift(tag);
+  this._tagsHistory=this._tagsHistory.splice(0,10);
+}
    searchTag(tag:string):void{
-    this._tagsHistory.unshift(tag);
+    if (tag.length===0) return;
+    this.ordenizaHistory(tag);
+    const params = new HttpParams()
+      .set('api_key',this.apiKey)
+      .set('limit',10)
+      .set('q',tag)
+
+
+
+    this.http.get(`${this.serviceUrl}/search`,{params})
+      .subscribe(resp=>{
+        console.log(resp);
+      })
+
   }
 }
